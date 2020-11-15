355e04d30b3abdd80780297ac1625e2d25c307da
xwwww <384450503@qq.com>
2020-11-15 22:05:45 +0800

    Book component

--- server/app.js
+++ server/app.js
@@ -2,8 +2,11 @@ const express = require('express')
 const { graphqlHTTP } = require('express-graphql')
 const schema = require('./schema/schema')
 require('./database/mongoose')()
+const cors = require('cors')
 
 const app = express()
+
+app.use(cors())
 
 app.use('/graphql', graphqlHTTP({
   schema,
